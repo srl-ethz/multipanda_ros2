a6148dbd9bb4a6bf9f25a04fa76b3599ed4bd6b7
Chenyu Yang <yangcyself@gmail.com>
2026-02-18 17:19:40 +0100

    config refine of description_pkg and load_gripper for real frankas

--- franka_bringup/launch/real/dual_multimode_franka.launch.py
+++ franka_bringup/launch/real/dual_multimode_franka.launch.py
@@ -232,12 +232,12 @@ def generate_launch_description():
                 use_fake_hardware_parameter_name)),
         DeclareLaunchArgument(
             load_gripper_1_parameter_name,
-            default_value='true',
+            default_value='false',
             description='Use Franka Gripper as an end-effector, otherwise, robot 1 is loaded '
                         'without an end-effector.'),
         DeclareLaunchArgument(
             load_gripper_2_parameter_name,
-            default_value='true',
+            default_value='false',
             description='Use Franka Gripper as an end-effector, otherwise, robot 2 is loaded '
                         'without an end-effector.'),
         DeclareLaunchArgument(
